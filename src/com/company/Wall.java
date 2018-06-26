2b4803f2b3cb5b89e6b7ccc3dd6272714694d06e
Mathias Vessberg <mathias.vessberg@gmail.com>
2018-06-26 14:04:30 +0200

    Add start logic to wall

--- src/com/company/Wall.java
+++ src/com/company/Wall.java
@@ -5,6 +5,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Wall {
@@ -12,17 +14,26 @@ public class Wall {
     int y = 0;
 
     public Wall(Terminal terminal, String filename) {
-        try {
 
+        try {
+            List<List<Boolean>> yWallList = new ArrayList<List<Boolean>>();
             Scanner scanner = new Scanner(new File(filename));
             while (scanner.hasNext()) {
+                List<Boolean> xWallList = new ArrayList<Boolean>();
                 char[] row = scanner.nextLine().toCharArray();
                 x = 0;
                 for (char c : row) {
                     terminal.moveCursor(x, y);
-                    terminal.putCharacter(c);
+                    if (c != ' ') {
+                        terminal.putCharacter(c);
+                        xWallList.add(true);
+                    }
+                    else {
+                        xWallList.add(false);
+                    }
                     x++;
                 }
+                yWallList.add(xWallList);
                 y++;
             }
 
