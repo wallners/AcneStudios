690cecd8e52fea4731fbef0a4f43d63e05adbc0a
Mathias Vessberg <mathias.vessberg@gmail.com>
2018-06-26 12:01:45 +0200

    refactorize wall class

--- src/com/company/Wall.java
+++ src/com/company/Wall.java
@@ -1,19 +1,36 @@
 package com.company;
 
 
+import com.googlecode.lanterna.terminal.Terminal;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Wall {
-    private void createWall() {
+    int x;
+    int y;
+
+    public Wall(Terminal terminal, String filename){
+
+        terminal.moveCursor(x, y);
+        terminal.putCharacter('O'); // refaktorisera
 
         try {
 
-            Scanner scanner = new Scanner(new File("wall.txt"));
+            Scanner scanner = new Scanner(new File(filename));
         } catch (FileNotFoundException e) {
             System.err.println("FileNotFoundException");
             System.exit(0);
         }
+
+
+    }
+
+
+    private void readWallFromFile() {
+
+
+
     }
 }
