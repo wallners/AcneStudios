50c07c551937794d0ebb588f33de75b8a0a45b86
wallner@strom <wallner@strom.se>
2018-06-27 10:12:51 +0200

    Add monster can't collide

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,44 +8,43 @@ import java.nio.charset.Charset;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         // write your code here
         Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
         terminal.enterPrivateMode();
 
         //Create the player and place it in the middle of the game
-        Player player = new Player(50  ,20);
+        Player player = new Player(50, 20);
 
         //Create the enemies
         Enemy[] enemies = new Enemy[4];
-        enemies[0] = new Enemy(5,5);
-        enemies[1] = new Enemy(15,5);
-        enemies[2] = new Enemy(5,15);
-        enemies[3] = new Enemy(15,15);
+        enemies[0] = new Enemy(5, 5);
+        enemies[1] = new Enemy(15, 5);
+        enemies[2] = new Enemy(5, 15);
+        enemies[3] = new Enemy(15, 15);
 
         terminal.setCursorVisible(false);
 
         boolean gameOver = false;
 
         Wall wall = new Wall(terminal, "maze-wall");
-        while(!gameOver){
+        while (!gameOver) {
 
             updateScreen(player, terminal, enemies, wall.isWall);
             movePlayer(player, terminal, wall);
             gameOver = gameLogic(player, enemies, wall);
         }
 
-        printText(5,5,"Game Over", terminal);
+        printText(5, 5, "Game Over", terminal);
     }
 
-private static void printText(int x, int y, String message, Terminal terminal) {
+    private static void printText(int x, int y, String message, Terminal terminal) {
 
-    for (int i=0;i<message.length();i++)
-    {
-        terminal.moveCursor(x++, y);
-        terminal.putCharacter(message.charAt(i));
+        for (int i = 0; i < message.length(); i++) {
+            terminal.moveCursor(x++, y);
+            terminal.putCharacter(message.charAt(i));
+        }
     }
-}
 
     // Move all the enemies and return true if a monster has killed the player
     private static boolean gameLogic(Player player, Enemy[] enemies, Wall wall) {
@@ -56,25 +55,29 @@ private static void printText(int x, int y, String message, Terminal terminal) {
 
             if (enemy.x != player.x) {
                 int diffx = player.x - enemy.x;
-                if (diffx > 0 && wall.isWall[enemy.y][enemy.x + 1]) {
+                if (diffx > 0 && !wall.isWall[enemy.y][enemy.x + 1]
+                        && !isOtherEnemyNearby(enemy, enemies, enemy.x + 1, enemy.y)) {
                     enemy.x = enemy.x + 1;
-                }
-                else if (wall.isWall[enemy.y][enemy.x - 1]) {
+                } else if (!wall.isWall[enemy.y][enemy.x - 1] && !isOtherEnemyNearby(enemy, enemies, enemy.x - 1, enemy.y)) {
                     enemy.x = enemy.x - 1;
                 }
             }
 
             if (enemy.y != player.y) {
                 int diffy = player.y - enemy.y;
-                if (diffy > 0 && wall.isWall[enemy.y + 1][enemy.x]) {
+                if (diffy > 0 && !wall.isWall[enemy.y + 1][enemy.x] && !isOtherEnemyNearby(enemy, enemies, enemy.x, enemy.y + 1)) {
                     enemy.y = enemy.y + 1;
-                }
-                else if (wall.isWall[enemy.y - 1][enemy.x]) {
+                } else if (!wall.isWall[enemy.y - 1][enemy.x] && !isOtherEnemyNearby(enemy, enemies, enemy.x, enemy.y - 1)) {
                     enemy.y = enemy.y - 1;
                 }
             }
 
-            //Check if we are game over?
+
+            //Render the player and the enemies
+
+
+            //Check the keyboard and move the player one step
+
             if (enemy.x == player.x && enemy.y == player.y)
                 return true;
         }
@@ -82,7 +85,6 @@ private static void printText(int x, int y, String message, Terminal terminal) {
     }
 
 
-    //Render the player and the enemies
     private static void updateScreen(Player player, Terminal terminal, Enemy[] enemies, boolean[][] isGameObject) {
 
 //        terminal.clearScreen();
@@ -90,7 +92,7 @@ private static void printText(int x, int y, String message, Terminal terminal) {
         for (int y = 0; y < 30; y++) {
             for (int x = 0; x < 100; x++) {
                 if (!isGameObject[y][x]) {
-                    terminal.moveCursor(x,y);
+                    terminal.moveCursor(x, y);
                     terminal.putCharacter(' ');
                 }
             }
@@ -107,51 +109,58 @@ private static void printText(int x, int y, String message, Terminal terminal) {
         }
 
         //Put the cursor on a fixed position after rendering to avoid flickering
-        terminal.moveCursor(0,0);
+        terminal.moveCursor(0, 0);
 
     }
 
-    //Check the keyboard and move the player one step
     private static void movePlayer(Player player, Terminal terminal, Wall wall) {
 
         // lägg in en try/catch
 
         //Wait for a key to be pressed
         Key key;
-        do{
+        do {
             //Thread.sleep(5);
-            key =terminal.readInput();
+            key = terminal.readInput();
         }
-        while(key == null);
+        while (key == null);
 
 
-        switch(key.getKind())
-        {
+        switch (key.getKind()) {
             case ArrowDown:
 
-                if (wall.isWall[player.y + 1][player.x]) {
+                if (!wall.isWall[player.y + 1][player.x]) {
                     player.y = player.y + 1;
                 }
                 break;
             case ArrowUp:
-                if (wall.isWall[player.y - 1][player.x]) {
+                if (!wall.isWall[player.y - 1][player.x]) {
                     player.y = player.y - 1;
                 }
                 break;
             case ArrowLeft:
-                if (wall.isWall[player.y][player.x - 1]) {
+                if (!wall.isWall[player.y][player.x - 1]) {
                     player.x = player.x - 1;
                 }
                 break;
             case ArrowRight:
-                if (wall.isWall[player.y][player.x + 1]) {
+                if (!wall.isWall[player.y][player.x + 1]) {
                     player.x = player.x + 1;
                 }
                 break;
         }
 
 
-        System.out.println(key.getCharacter()+ " " + key.getKind());
+        System.out.println(key.getCharacter() + " " + key.getKind());
     }
-}
 
+    private static boolean isOtherEnemyNearby(Enemy enemy, Enemy[] enemies, int nextX, int nextY) {
+        for (Enemy otherEnemy : enemies) {
+            if (nextX - otherEnemy.x == 0 && nextY - otherEnemy.y == 0 && enemy != otherEnemy) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
