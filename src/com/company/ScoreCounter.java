6a661a6786bc44ccabaa47b6d5ef1d2c486e6769
Mathias Vessberg <mathias.vessberg@gmail.com>
2018-06-27 10:13:56 +0200

    add ScoreCounter

---
+++ src/com/company/ScoreCounter.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ScoreCounter {
+    int x = 0;
+    int score = 0;
+
+    public ScoreCounter(Terminal terminal, int x) {
+
+    }
+}
